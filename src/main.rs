94aa2624230d93d123901ba723d39d4465e3b1dd
Owanikin <oderindeife@gmail.com>
2022-09-21 18:34:09 +0100

    threads run at different time based on where join is called

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,12 @@ fn main() {
         }
     });
 
+    handle.join().unwrap();
+    
     for i in 1..5 {
         println!("Hi number {} from the main thread!", i);
         thread::sleep(Duration::from_millis(1));
     }
 
-    handle.join().unwrap();
+    
 }
