271761b37ed2be874a40502fce3c850466cb330d
Owanikin <oderindeife@gmail.com>
2022-09-23 18:12:23 +0100

    creating multiple producers by cloning the transmitter

--- src/main.rs
+++ src/main.rs
@@ -5,26 +5,37 @@ use std::time::Duration;
 fn main() {
     let (tx, rx) = mpsc::channel();
 
-    thread::spawn(move || {
-        let vals = vec![
-            String::from("Hello"),
-            String::from("from"),
-            String::from("the"),
-            String::from("thread"),
-        ];
+let tx1 = mpsc::Sender::clone(&tx);
+thread::spawn(move || {
+    let vals = vec![
+        String::from("hi"),
+        String::from("from"),
+        String::from("the"),
+        String::from("thread"),
+    ];
 
-        for val in vals {
-            tx.send(val).unwrap();
-            thread::sleep(Duration::from_secs(1));
-        }
-        // let val = String::from("Data");
-        // tx.send(val).unwrap();
-        // println!("val is {}", val);
-    });
-
-    // let received = rx.recv().unwrap();
-    // println!("Got: {}", received);
-    for received in rx {
-        println!("Got: {}", received);
+    for val in vals {
+        tx1.send(val).unwrap();
+        thread::sleep(Duration::from_secs(1));
     }
+});
+
+thread::spawn(move || {
+    let vals = vec![
+        String::from("more"),
+        String::from("messages"),
+        String::from("for"),
+        String::from("you"),
+    ];
+
+    for val in vals {
+        tx.send(val).unwrap();
+        thread::sleep(Duration::from_secs(1));
+    }
+});
+
+for received in rx {
+    println!("Got: {}", received);
+}
+
 }
