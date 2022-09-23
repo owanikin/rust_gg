b281e03ee83b7ae3e1d492498682a7b872a9f7eb
Owanikin <oderindeife@gmail.com>
2022-09-23 17:10:55 +0100

    receiving value and printing it

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,10 @@ fn main() {
     let (tx, rx) = mpsc::channel();
 
     thread::spawn(move || {
-        let val = String::from("hi")
+        let val = String::from("Data");
         tx.send(val).unwrap()
-    })
+    });
+
+    let received = rx.recv().unwrap();
+    println!("Got: {}", received);
 }
