2e985c23c9f998b63e36062f3c11b3edc82e22a2
Owanikin <oderindeife@gmail.com>
2022-09-25 20:00:31 +0100

    using atomic referencing

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use std::rc::Rc;
-use std::sync::Mutex;
+use std::sync::{Mutex, Arc};
 use std::thread;
 
 fn main() {
-    let counter = Rc::new(Mutex::new(0));
+    let counter = Arc::new(Mutex::new(0));
     let mut handles = vec![];
 
     // let handle = thread::spawn(move || {
@@ -19,7 +19,7 @@ fn main() {
     // handles.push(handle2);
 
     for _ in 0..10 {
-        let counter = Rc::clone(&counter);
+        let counter = Arc::clone(&counter);
         let handle = thread::spawn(move || {
             let mut num = counter.lock().unwrap();
             *num += 1;
