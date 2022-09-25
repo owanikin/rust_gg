a8ee70cc659552f2412cb61b7e9400ccfc007e61
Owanikin <oderindeife@gmail.com>
2022-09-25 08:59:10 +0100

    The API of Mutex<T>

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,12 @@
-use std::thread;
-use std::sync::mpsc;
-use std::time::Duration;
+use std::sync::Mutex;
 
 fn main() {
-    let (tx, rx) = mpsc::channel();
+    let m = Mutex::new(5);
 
-let tx1 = mpsc::Sender::clone(&tx);
-thread::spawn(move || {
-    let vals = vec![
-        String::from("hi"),
-        String::from("from"),
-        String::from("the"),
-        String::from("thread"),
-    ];
-
-    for val in vals {
-        tx1.send(val).unwrap();
-        thread::sleep(Duration::from_secs(1));
+    {
+        let mut num = m.lock().unwrap();
+        *num = 6;
     }
-});
 
-thread::spawn(move || {
-    let vals = vec![
-        String::from("more"),
-        String::from("messages"),
-        String::from("for"),
-        String::from("you"),
-    ];
-
-    for val in vals {
-        tx.send(val).unwrap();
-        thread::sleep(Duration::from_secs(1));
-    }
-});
-
-for received in rx {
-    println!("Got: {}", received);
-}
-
+    println!("n = {:?}", m);
 }
