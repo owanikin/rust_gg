805ee1a7e2c9898f3a6171a326a57c180a0d7a69
Owanikin <oderindeife@gmail.com>
2022-09-25 19:52:04 +0100

    multiple ownership with multiple threads

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,31 @@
+use std::rc::Rc;
 use std::sync::Mutex;
 use std::thread;
 
 fn main() {
-    let counter = Mutex::new(0);
+    let counter = Rc::new(Mutex::new(0));
     let mut handles = vec![];
 
-    let handle = thread::spawn(move || {
-        let mut num = counter.lock().unwrap();
-        *num += 1;
-    });
-    handles.push(handle);
+    // let handle = thread::spawn(move || {
+    //     let mut num = counter.lock().unwrap();
+    //     *num += 1;
+    // });
+    // handles.push(handle);
 
-    let handle2 = thread::spawn(move || {
-        let mut num2 = counter.lock().unwrap();
-        *num2 += 1;
-    });
-    handles.push(handle2);
+    // let handle2 = thread::spawn(move || {
+    //     let mut num2 = counter.lock().unwrap();
+    //     *num2 += 1;
+    // });
+    // handles.push(handle2);
+
+    for _ in 0..10 {
+        let counter = Rc::clone(&counter);
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+            *num += 1;
+        });
+        handles.push(handle);
+    }
 
     for handle in handles {
         handle.join().unwrap();
