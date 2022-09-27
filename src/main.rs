9feaccb277e08b0c4a8178da2606ade5e7accbc6
Owanikin <oderindeife@gmail.com>
2022-09-27 15:54:00 +0100

    calling functions from other languages

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+#[no_mangle]
+pub extern "C" fn call_from_c() {
+    println!("Just called a Rust function from C!")
+}
+
 extern "C" {
     fn abs(input: i32) -> i32;
 }
@@ -7,3 +12,4 @@ fn main() {
         println!("Absolute value of -3 according to C: {}", abs(-3));
     }
 }
+
